de64f8a9d04464ec549c60f1de4b5f58517bc25b
giangann <70595717+giangann@users.noreply.github.com>
2021-05-10 17:13:07 +0700

    my commit at 5:12 pm
    I don't know what this text area do :((

---
+++ App1005/src/App.js
@@ -0,0 +1,21 @@
+import classes from "./App.module.css";
+import "antd/dist/antd.css";
+import TaskInput from "./TaskInput";
+import Title from "antd/lib/typography/Title";
+import TodoList from "./TodoList";
+
+function App() {
+  return (
+    <div className={classes.app}>
+      <Title className={classes.header}>Todo app</Title>
+      <div className={classes.taskInputContainer}>
+        <TaskInput />  
+      </div>
+      <section className={classes.taskListContainer}>
+        <TodoList />
+      </section>
+    </div>    
+  );
+}
+
+export default App;
